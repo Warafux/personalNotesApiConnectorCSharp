38bd053f9c63084ec33923ed4ae931e864b428d9
Alex Paredes Martinez <alexparedesmartinez@gmail.com>
2017-12-08 21:19:02 +0100

    First version all ok

---
+++ personalNotesApiConnectorCSharp/note.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace personalNotesApp
+{
+    public class note
+    {
+        private string title;
+        private string text;
+        private int id;
+        private String dateCreation;
+
+        public note()
+        {
+            this.title = "";
+            this.text = "";
+            this.id = 0;
+        }
+        public note(int id, string title, string text, string dateCreation)
+        {
+            this.id = id;
+            this.title = title;
+            this.text = text;
+            this.dateCreation = dateCreation;
+        }
+        public string getTitle()
+        {
+            return this.title;
+        }
+        public string getText()
+        {
+            return this.text;
+        }
+        public int getId()
+        {
+            return this.id;
+        }
+        public string getDateCreation()
+        {
+            return this.dateCreation;
+        }
+
+        public void setTitle(string title)
+        {
+            this.title = title;
+        }
+        public void setText(string text)
+        {
+            this.text = text;
+        }
+    }
+}
---
+++ personalNotesApiConnectorCSharp/personalNotesApiConnector.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using Newtonsoft.Json.Schema;
+using Newtonsoft.Json.Linq;
+
+namespace personalNotesApp
+{
+    public class personalNotesAppApiConnector
+    {
+        private static readonly HttpClient client = new HttpClient();
+        private string apiUrl = "https://personalnotesapi.alexparedes.ovh/";
+        private string token = "";
+        private Boolean isLogged = false;
+        public personalNotesAppApiConnector()
+        {
+            client.Timeout = TimeSpan.FromSeconds(2);
+        }
+        public void deleteNote(string noteId, Action<bool, string> callback)
+        {
+            JObject jsonValues = JObject.FromObject(
+                new
+                {
+                    query = "deleteNote",
+                    token = this.token,
+                    noteId = noteId
+                }
+            );
+            this.genericRequestMethod(jsonValues, callback);
+        }
+        public void getStatus(Action<bool, string> callback)
+        {
+            JObject jsonValues = JObject.FromObject(
+                new
+                {
+                    query = "status",
+                    token = this.token,
+                }
+            );
+            this.genericRequestMethod(jsonValues, callback);
+        }
+        public void attemptLogin(string username, string password, Action<bool, string> callback)
+        {
+            JObject jsonValues = JObject.FromObject(
+                new {
+                    query = "login",
+                    username = username,
+                    password = password
+                }
+            );
+            this.genericRequestMethod(jsonValues, callback);
+        }
+        public void createUser(string username, string password, string email, Action<bool, string> callback)
+        {
+            JObject jsonValues = JObject.FromObject(
+                new
+                {
+                    query = "createUser",
+                    username = username,
+                    email = email,
+                    password = password
+                }
+            );
+            this.genericRequestMethod(jsonValues, callback);
+        }
+        public void getNotes(Action<bool, string> callback)
+        {
+            JObject jsonValues = JObject.FromObject(
+                new
+                {
+                    query = "notes",
+                    token = this.token
+                }
+            );
+            this.genericRequestMethod(jsonValues, callback);
+        }
+        public void editNote(string noteId, string newTitle, string newText, Action<bool, string> callback)
+        {
+            JObject jsonValues = JObject.FromObject(
+                new
+                {
+                    query = "editNote",
+                    token = this.token,
+                    noteId = noteId,
+                    text = newText,
+                    title = newTitle
+                }
+            );
+            this.genericRequestMethod(jsonValues, callback);
+        }
+        public void newNote(string newTitle, string newText, Action<bool, string> callback)
+        {
+            JObject jsonValues = JObject.FromObject(
+                new
+                {
+                    query = "newNote",
+                    token = this.token,
+                    text = newText,
+                    title = newTitle
+                }
+            );
+            this.genericRequestMethod(jsonValues, callback);
+        }
+        private async void genericRequestMethod(JObject jsonValues, Action<bool, string> callback)
+        {
+            var content = new StringContent(jsonValues.ToString(), Encoding.UTF8, "application/json");
+            try
+            {
+                var response = await client.PostAsync(this.apiUrl, content);
+
+                string responseString = await response.Content.ReadAsStringAsync();
+                callback(true, responseString);
+
+            }
+            catch (Exception e)
+            {
+                callback(false, "Connection error.");
+            }
+        }
+        public void setToken(string token)
+        {
+            this.token = token;
+        }
+        public string getToken()
+        {
+            return this.token;
+        }
+        public string getApiURL()
+        {
+            return this.apiUrl;
+        }
+    }
+}
